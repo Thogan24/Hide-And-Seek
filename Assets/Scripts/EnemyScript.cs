008bda793ec0e4afbb9c6197d56deec79df41762
Thogan24 <thogan1924@gmail.com>
2022-08-20 14:20:56 -0400

    Made a way to track the Damage per second

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public float EnemyHealth = 999999f;
+    public float time = 0f;
+    public float DPS = 0f;
 
     void Start()
     {
@@ -11,7 +13,14 @@ public class EnemyScript : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(EnemyHealth);
+        time += Time.deltaTime;
+        if (time > 1f)
+        {
+            time = 0f;
+            DPS = 999999 - EnemyHealth;
+            EnemyHealth = 999999f;
+        }
+        Debug.Log(DPS);
     }
 
     public void TakeDamage()
