ad03b0dbfcd6bb68ee136bffa48269a4903e2b7f
Thogan24 <thogan1924@gmail.com>
2022-08-26 17:44:47 -0400

    Fixxed up DPSDisplayer which uses a 3DText Object

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemyScript : MonoBehaviour
 {
     public float EnemyHealth = 999999f;
     public float time = 0f;
     public float DPS = 0f;
+    public TextMesh DPSDisplayer;
+    public GameObject DPSDisplayerObject;
 
     void Start()
     {
-        
+        DPSDisplayer = DPSDisplayerObject.GetComponent<TextMesh>() as TextMesh;
     }
 
     void Update()
@@ -21,6 +24,8 @@ public class EnemyScript : MonoBehaviour
             EnemyHealth = 999999f;
         }
         Debug.Log(DPS);
+
+        DPSDisplayer.text = DPS.ToString();
     }
 
     public void TakeDamage()
